b6b457f517d9b0a518e6798801c135a4020ae96c
Rezimioszek <lukasz.swiatek1996@gmail.com>
2025-07-29 10:40:02 +0200

    more style

--- app.js
+++ app.js
@@ -157,11 +157,32 @@ function isRecipeFavorite(title) {
 
 function updateFavoriteIcon(title) {
   const icon = document.getElementById('addFav');
+  const favIcon = document.getElementById('favIcon');
   if (!icon) return;
 
   if (isRecipeFavorite(title)) {
-    icon.textContent = '❤️'; // w ulubionych
+    // w ulubionych
+    favIcon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-500" fill="currentColor" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 
+        8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 
+        4.5 2.09C13.09 3.81 14.76 3 16.5 3 
+        19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 
+        11.54L12 21.35z" clip-rule="evenodd"/>
+      </svg>
+    `;
   } else {
-    icon.textContent = '🤍'; // nie w ulubionych
+    //icon.textContent = '🤍'; // nie w ulubionych
+    favIcon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 
+        8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 
+        4.5 2.09C13.09 3.81 14.76 3 16.5 3 
+        19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 
+        11.54L12 21.35z"
+              />
+      </svg>
+    `;
   }
 }
